750fdec87e2dc8ec2b4473095206898219051a89
ryo464 <ryokojowhite@gmail.com>
2026-05-18 19:14:15 +0900

    ページ間の遷移を可能にした

--- app/buildings/page.tsx
+++ app/buildings/page.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link"
+
+
 export default function BuildingsPage() {
   return (
     <div>
       <h1>トイレ一覧ページ</h1>
+
+      <Link href="/mypage">
+        マイページへ
+      </Link>
     </div>
   )
 }
--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link"
+
+
 export default function MyPage() {
   return (
     <div>
       <h1>マイページ</h1>
+
+      <Link href="/buildings">
+        トイレ一覧へ
+      </Link>
     </div>
   )
 }
